e7b5f7c899d59ebafc6bd20368978337bb749e99
Aldair <aldair.villaruel@resser.com>
2023-01-30 04:22:12 -0600

    Bascula

--- src/pages/Bascula.tsx
+++ src/pages/Bascula.tsx
@@ -1,10 +1,11 @@
 import { Col, Row } from "antd";
+import BasculaMain from "../components/Bascula/BasculaMain";
 
 export const Bascula = () => {
     return (
       <Row style={{width: '100%'}}>
           <Col span={24} style={{width: '100%', height: '10vh', placeContent: 'center', display: 'flex',}}>
-            <h1> Bascula </h1>
+            <BasculaMain/>
           </Col>
       </Row>
     );
