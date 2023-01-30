148549f3fcec36a94bf2292f29a4ba63aeae0379
Aldair <aldair.villaruel@resser.com>
2023-01-30 04:11:43 -0600

    Impresora de tickets

---
+++ src/components/ImpresoraTickets/TicketPrinterMain.jsx
@@ -0,0 +1,53 @@
+/* eslint-disable no-unused-vars */
+import React, { useRef } from 'react';
+import { Button, message } from 'antd';
+import ReactToPrint from 'react-to-print';
+import Ticket from './Ticket/Ticket';
+
+
+const TicketPrinter = () => {
+  const [_, contextHolder] = message.useMessage();
+  const componentRef = useRef();
+
+  const pageStyle = `
+      @page: {
+        size: 100%;
+      }
+      @media all {
+        .page-break {
+          display: none;
+        }
+      }
+      @media print {
+        .page-break {
+          margin-top: 1rem;
+          display: block;
+          page-break-before: auto;
+        }
+      }
+    `;
+
+  return (
+    <div className='container mx-auto border-gray-200 dark:bg-gray-900 m-4' style={{marginLeft: '25px'}}>
+      {contextHolder}
+      <h1 className='text-2xl mt-10 m-4'>
+        Al presionar el boton situado en la parte inferior se mandará a imprimir una hoja de pruebas a la impresora de tickets.
+      </h1>
+      <Ticket ref={componentRef} className='mt-5 m-4'/>
+      <ReactToPrint
+        trigger={() =>
+          <Button className='oculto-impresion mt-5 m-4'
+            onClick={() => message.loading('Preparando impresión', 2.5)}
+          >
+            Presiona para mandar la prueba a impresion
+          </Button>}
+        content={() => componentRef.current}
+        pageStyle={pageStyle}
+        onAfterPrint={() => message.success('Impresión lista', 2.5)}
+        onPrintError={() => message.error('Error al imprimir', 2.5)}
+      />
+    </div>
+  );
+};
+
+export default TicketPrinter;
--- src/pages/ImpresoraEtiquetas.tsx
+++ src/pages/ImpresoraEtiquetas.tsx
@@ -1,10 +1,11 @@
 import { Col, Row } from "antd";
+import TicketPrinter from "../components/ImpresoraTickets/TicketPrinterMain";
 
 export const ImpresoraEtiquetas = () => {
     return (
       <Row style={{width: '100%'}}>
           <Col span={24} style={{width: '100%', height: '10vh', placeContent: 'center', display: 'flex',}}>
-            <h1> Impresora de Etiquetas </h1>
+            <TicketPrinter/>
           </Col>
       </Row>
     );
